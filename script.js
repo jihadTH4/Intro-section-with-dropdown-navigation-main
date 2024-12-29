64538f5dfbdfaa1bfb91f8e3cd12283c64ecb6be
Jihad Thabit <thabtjhad37@gmail.com>
2024-12-29 15:54:13 +0200

    Merged remote changes with resolved conflicts

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const openMenuBtn = document.querySelector('.open');
 const closeMenuBtn = document.querySelector('.close');
 const navBar = document.querySelector('nav');
@@ -20,26 +19,4 @@ overlay.addEventListener('click', () => {
     navBar.style.transform = 'translateX(100vh)';
     overlay.style.display = 'none';
 });
-=======
-const openMenuBtn = document.querySelector('.open');
-const closeMenuBtn = document.querySelector('.close');
-const navBar = document.querySelector('nav');
-const overlay = document.querySelector('.overlay');
 
-openMenuBtn.addEventListener('click', () => {
-    navBar.style.display = 'flex';
-    overlay.style.display = 'block';
-})
-
-closeMenuBtn.addEventListener('click', () => {
-    navBar.style.display = 'none';
-    overlay.style.display = 'none';
-})
-
-
-// Optionally, close the menu by clicking the overlay
-overlay.addEventListener('click', () => {
-    navBar.style.display = 'none';
-    overlay.style.display = 'none';
-});
->>>>>>> origin/main
